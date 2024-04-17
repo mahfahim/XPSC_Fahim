685636d8225ae8ceddd42b6bc224c7f462206db8
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-04-17 23:16:47 +0600

    Add files via upload

---
+++ Week4/Day4/Q3.cpp
@@ -0,0 +1,43 @@
+//********* Bismillahir Rahmanir Rahim *********
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define ldb long double
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+     int t;                 
+     cin>>t;
+    while(t--){
+        int n;      
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+ 
+ 
+        if(n==1){
+            cout<<0<<endl;
+            continue;
+        }
+        int ans=a[n-1]-a[0];
+ 
+        for(int i=1;i<=n-1;i++){
+            ans=max(ans,a[i]-a[0]);
+        }
+        for(int i=0;i<=n-2;i++){
+            ans=max(ans,a[n-1]-a[i]);
+        }
+        for(int i=1;i<=n-1;i++){
+            ans=max(ans,a[i-1]-a[i]);
+        }
+ 
+        cout<<ans<<endl;
+    }
+    //********* Alhamdulillah *********
+}
