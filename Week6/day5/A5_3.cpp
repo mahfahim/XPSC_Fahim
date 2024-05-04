b83ce753324f23d794349e0461ff4da1b2c33b9d
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-05-04 22:30:02 +0600

    Add files via upload

---
+++ Week6/day5/A5_3.cpp
@@ -0,0 +1,28 @@
+//********* Bismillahir Rahmanir Rahim *********
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define ldb long double
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,k;
+    cin >> n >> k;
+    vector<int> v(n);
+    vector<int> q(n);
+    for(int i=0;i<n;i++) cin >> v[i];
+    for(int i=0;i<k;i++) 
+    {
+        cin >> q[i];
+        auto it = lower_bound(v.begin(),v.end(),q[i]);
+        cout <<(it - v.begin() ) + 1 << endl;
+    }
+    
+    return 0;
+    //********* Alhamdulillah *********
+}
