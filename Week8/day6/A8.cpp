ea3449c9935bbf2464b7ed0a8a77442faa6f3c69
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-05-22 22:55:32 +0600

    Add files via upload

---
+++ Week8/day6/A8.cpp
@@ -0,0 +1,32 @@
+//********* Bismillahir Rahmanir Rahim *********
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define ldb long double
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    if(n%2==0)
+    {
+        cout << n/2 << endl;
+        for(int i=1;i<=n/2;i++) cout << 2 << " ";
+
+        cout << endl;
+    }
+    else 
+    {
+        cout << n/2 << endl;
+        for(int i=1;i<=n/2 - 1;i++) cout << 2 << " ";
+
+        cout<< 3 << endl;
+    }
+    return 0;
+    //********* Alhamdulillah *********
+}
