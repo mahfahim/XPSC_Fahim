a00f383ff42630af8a5eb197bac92d14a334f464
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-07-10 21:54:29 +0600

    Create 1.cpp

---
+++ Week12/Day2/1.cpp
@@ -0,0 +1,20 @@
+//********* Bismillahir Rahmanir Rahim *********
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define ldb long double
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x;
+    cin >> x;
+    if(x<=12) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    
+    return 0;
+    //********* Alhamdulillah *********
+}
