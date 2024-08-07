ec4db87f28a5f822b32bf4e7d63c9dced91aaf05
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-08-07 20:46:54 +0600

    Create s146_1.cpp

---
+++ Week12/s146_1.cpp
@@ -0,0 +1,24 @@
+//********* Bismillahir Rahmanir Rahim *********
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define endl "\n"
+#define ll long long
+#define ldb long double
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int g,s,b;
+    cin >> g >> s >> b;
+    int sum = (5-g) + (5-s) + (5-b);
+    cout << sum << endl;
+    
+    return 0;
+    //********* Alhamdulillah *********
+}
